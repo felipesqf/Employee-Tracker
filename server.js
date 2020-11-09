cfc009a7c6071818b9c31436cdaf3392bbaa97c0
^Felipe <felipesqf@gmail.com>
2020-11-09 18:32:53 +1100

    adding roles and department

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 var mysql = require("mysql");
-
 const choices = ["Add departments","Add roles", "Add employees", "View departments", "View roles", "View Employees", "Update employee roles" ]
+let departments;
+let managers;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -14,11 +15,20 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
+    
     init();
 });
 
+selectDepartments = () =>{
+    connection.query("SELECT * FROM department", function(err, res) {
+        if (err) throw err;
+        departments =  res;
+    })
+}
+
 //propmt user question
 init = () =>{
+    selectDepartments();
     inquirer.prompt([{
         type: "rawlist",
         name: "choice",
@@ -29,16 +39,18 @@ init = () =>{
     .then((choices) => {
         switch(choices.choice) {
             case "Add departments":
-              // code block
+                insertDepartment();
               break;
             case "Add roles":
-              // code block
+              
+                insertRole();
               break;
             case "Add employees":
             // code block
                 break;
             case "View departments":
-                selectDepartments();
+                console.table(departments)
+                init();
               break;
             case "View roles":
                 selectRoles();
@@ -61,13 +73,13 @@ selectRoles = () => {
     });
 }
 
-selectDepartments = () => {
-    connection.query("SELECT * FROM department", function(err, res) {
-        if (err) throw err;
-        console.table(res)
-        init();
-    });
-}
+// selectDepartments = () => {
+//     connection.query("SELECT * FROM department", function(err, res) {
+//         if (err) throw err;
+//         console.table(res)
+//         init();
+//     });
+// }
 
 selectEmployees = () => {
     connection.query("SELECT employee.id, employee.first_name, employee.last_name, emp_role.title, emp_role.salary, department.name FROM employee JOIN emp_role ON employee.role_id = emp_role.id JOIN department ON emp_role.department_id = department.id", function(err, res) {
@@ -75,4 +87,48 @@ selectEmployees = () => {
         console.table(res)
         init();
     });
+}
+
+insertDepartment = () =>{
+    inquirer.prompt([{
+        type: "input",
+        name: "department",
+        message: "What is the department name?",
+        }
+    ])
+    .then((newDepartment) => {
+        connection.query(`INSERT INTO department (name) VALUES ("${newDepartment.department}")`,function(err, res) {
+            if (err) throw err;
+            console.log(`Department ${newDepartment.department} added successfully`)
+            init();
+        }); 
+    })
+}
+
+insertRole = () =>{
+    inquirer.prompt([
+        {
+        type: "input",
+        name: "roleName",
+        message: "Please enter the name of the Role:"
+        },
+        {
+        type: "input",
+        name: "salary",
+        message: "Please enter the salary amount"
+        },
+        {
+        type: "list",
+        name: "departmentId",
+        message: "Please Select the Department",
+        choices: departments
+        }
+    ])
+    .then((newRole) => {
+        connection.query(`INSERT INTO emp_role (title, salary, department_id) VALUES ("${newRole.roleName}", ${newRole.salary}, ${newRole.departmentId})`,function(err, res) {
+            if (err) throw err;
+            console.log(`Department ${newDepartment.department} added successfully`)
+            init();
+        }); 
+    })
 }
